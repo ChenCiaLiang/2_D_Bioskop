29ef9645771dcb4eea6fde5e5df9c9f7e982e72e
ChenCiaLiang <aliang334chen@gmail.com>
2024-10-02 22:47:01 +0700

    First commit tubez
    Login & Register

---
+++ tubez/lib/widgets/login_options.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:tubez/service/directToLink.dart';
+
+class LoginOption extends StatelessWidget {
+  const LoginOption({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        BuildButton(
+          iconImage: Image(
+              height: 35, width: 35, image: AssetImage('images/facebook.png')),
+          onPressed: () {
+            Direct.launchURL('https://www.instagram.com/');
+          },
+        ),
+        BuildButton(
+          iconImage: Image(
+              height: 35, width: 35, image: AssetImage('images/gugel.png')),
+          onPressed: () {
+            Direct.launchURL('https://www.instagram.com/');
+          },
+        ),
+        BuildButton(
+          iconImage: Image(
+              height: 35, width: 35, image: AssetImage('images/discord.png')),
+          onPressed: () {
+            Direct.launchURL('https://www.instagram.com/');
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class BuildButton extends StatelessWidget {
+  final Image iconImage;
+  BuildButton({required this.iconImage, required Null Function() onPressed});
+  @override
+  Widget build(BuildContext context) {
+    var mediaQuery = MediaQuery.of(context).size;
+    return Container(
+      height: mediaQuery.height * 0.06,
+      width: mediaQuery.width * 0.16,
+      child: iconImage,
+    );
+  }
+}
