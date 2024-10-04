4141d0beafd0a059cebef9e12bc9ef93c132b687
ChenCiaLiang <aliang334chen@gmail.com>
2024-10-04 18:40:00 +0700

    List Menu Promo dan Menu Biasa(isiMenu.dart) pada ListScreen(list.dart)

--- tubez/lib/screens/list.dart
+++ tubez/lib/screens/list.dart
@@ -1,5 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:tubez/screens/isiList.dart';
+import 'package:tubez/screens/isiMenu.dart';
 
 class ListScreen extends StatelessWidget {
   const ListScreen({super.key});
@@ -10,15 +11,39 @@ class ListScreen extends StatelessWidget {
       child: Scaffold(
         backgroundColor: const Color.fromARGB(255, 255, 255, 255),
         appBar: AppBar(
-          title: const Text('LIST FILM'),
+          title: const Text('LIST Menu'),
           backgroundColor: const Color.fromARGB(255, 166, 149, 0),
         ),
-        body: const SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [IsiList()],
+        body: Column(
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text('PROMO', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)
           ),
-        ),
+          Expanded(
+            flex: 1,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: IsiMenu.sampleContent.menuPromo,
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child:  Text('Menu', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)
+          ),
+          Expanded(
+            flex: 1,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                children: IsiMenu.sampleContent.menuBiasa,
+              ),
+            ),
+          ),
+        ],
+      ),
       ),
     );
   }
