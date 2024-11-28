5e82d4b2e4321b480e0e48e54252278a6f3fc8aa
Alphonsus01 <220711671@strudents.uajy.ac.id>
2024-11-28 19:36:38 +0700

    update history dan revirew controller

--- database/app/Http/Controllers/HistoryController.php
+++ database/app/Http/Controllers/HistoryController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
+use App\Models\History;
+use Illuminate\Support\Facades\Storage;
 
 class HistoryController extends Controller
 {
-    //
+    /**
+     * index
+     * 
+     * @return void
+     */
+    public function show()
+    {
+        //get history
+        $history = History::all();
+        //render view with posts
+        return view('history.show', compact('history'));
+    }
 }
