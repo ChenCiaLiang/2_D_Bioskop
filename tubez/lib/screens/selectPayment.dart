74ac5bec13a4c162824cc55655fc55c2cd47f9a9
Alphonsus01 <220711671@strudents.uajy.ac.id>
2024-11-29 15:52:02 +0700

    tampilan select payment

---
+++ tubez/lib/screens/selectPayment.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'dart:ui';
+import 'package:tubez/widgets/MovieDetailWidgets/BackButton.dart';
+
+class selectPaymentScreen extends StatefulWidget {
+  const selectPaymentScreen({super.key});
+
+  @override
+  State<selectPaymentScreen> createState() => _selectPaymentScreenState();
+}
+
+class _selectPaymentScreenState extends State<selectPaymentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+            child: Stack(
+      children: [
+        BackButtonWidget(),
+        Positioned(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                        children: [
+                          Container(
+                            child:
+                            )],
+                      ),
+            ))
+      ],
+    )));
+  }
+}
